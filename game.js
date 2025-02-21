395ee391a3d9ca07b2967dd8e2773e7d5def774d
Benedict Ortiz <benortiz1391@gmail.com>
2025-02-20 21:35:53 -0500

    added restart button

---
+++ game.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const gameGrid = document.getElementById("gameGrid")
+    const restartButton = document.getElementById("restartButton")
+
+
+
+
+
+
+
+
+
+
+
+
+    restartButton.addEventListener("click", () => {
+        console.log("restarting game")
+    })
+
+
+});
--- script.js
+++ script.js
@@ -1,9 +1,2 @@
 const button1 = document.getElementById("button1")
 const counter = document.getElementById("counter")
-
-
-let count = 0;
-button1.addEventListener("click", () => {
-    count++;
-    counter.innerText = count
-});
